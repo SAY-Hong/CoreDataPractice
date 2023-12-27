ccb1c057fbfd894fc18be8fd9ecbca34d65ff67c
SAY-Hong <bobseahee@gmail.com>
2023-12-27 13:32:31 +0900

    Feat: 검색 가능 코드

--- SwiftUI-CoreDataDemo/SwiftUI-CoreDataDemo/ContentView.swift
+++ SwiftUI-CoreDataDemo/SwiftUI-CoreDataDemo/ContentView.swift
@@ -37,6 +37,11 @@ struct ContentView: View {
                         addProduct()
                     }
                     Spacer()
+                    
+                    NavigationLink(destination: ResultView(name: name, viewContext: viewContext)) {
+                        Text("Find")
+                    }
+                    Spacer()
                     Button("Clear") {
                         name = ""
                         quantity = ""
@@ -45,17 +50,6 @@ struct ContentView: View {
                 }
                 .padding()
                 .frame(maxWidth: .infinity)
-                
-                List {
-                    ForEach(products) { product in
-                        HStack {
-                            Text(product.name ?? "Not found")
-                            Spacer()
-                            Text(product.quantity ?? "Not found")
-                        }
-                    }
-                }
-                .navigationTitle("Product Database")
             }
             .padding()
             .textFieldStyle(RoundedBorderTextFieldStyle())
@@ -146,6 +140,41 @@ struct ContentView: View {
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
+        }
+    }
+}
+
+struct ResultView: View {
+    var name: String
+    var viewContext: NSManagedObjectContext
+    // 제품의 검색 결과가 List에 표시될 상태 프로퍼티
+    @State var matches: [Product]?
+    var body: some View {
+        VStack {
+            List {
+                ForEach(matches ?? []) { match in
+                    HStack {
+                        Text(match.name ?? "Not found")
+                        Spacer()
+                        Text(match.quantity ?? "Not found")
+                    }
+                }
+            }
+            .navigationTitle("Product Database")
+        }
+        // 검색할 떄는 비동기로!
+        .task {
+            // Product 엔티티에서 fetchRequest 인스턴스를 가져온다.
+            let fetchRequest: NSFetchRequest<Product> = Product.fetchRequest()
+            
+            // Product 엔티티에서 검색 조건을 정의
+            // CONTAINS: 검색에서 지정된 텍스트를 포함하는 모든 제품을 검색.
+            fetchRequest.entity = Product.entity()
+            //predicate -> 검색 조건
+            fetchRequest.predicate = NSPredicate(format: "name CONTAINS %@", name)
+            
+            // 검색 결과를 matches에 할당.
+            matches = try? viewContext.fetch(fetchRequest)
         }
     }
 }
