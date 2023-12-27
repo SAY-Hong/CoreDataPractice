0482d7e18083f2d6fac62e1667987b7662f9f0d3
SAY-Hong <bobseahee@gmail.com>
2023-12-27 11:57:57 +0900

    Add: CoreData swiftUI

---
+++ SwiftUI-CoreDataDemo/SwiftUI-CoreDataDemo/Persistence.swift
@@ -0,0 +1,45 @@
+//
+//  Persistence.swift
+//  SwiftUI-CoreDataDemo
+//
+//  Created by 홍세희 on 2023/12/27.
+//
+
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+
+    static var preview: PersistenceController = {
+        let result = PersistenceController(inMemory: true)
+        let viewContext = result.container.viewContext
+        for _ in 0..<10 {
+            let newItem = Item(context: viewContext) // CoreData파일 - Entity - Item 부분이 여기로!
+            newItem.timestamp = Date()
+        }
+        do {
+            try viewContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+        return result
+    }()
+
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "SwiftUI_CoreDataDemo")
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+}
---
+++ SwiftUI-CoreDataDemo/SwiftUI-CoreDataDemo/SwiftUI_CoreDataDemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUI_CoreDataDemoApp.swift
+//  SwiftUI-CoreDataDemo
+//
+//  Created by 홍세희 on 2023/12/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CoreDataDemoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
